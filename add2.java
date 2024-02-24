9fa6fb54505ab67d76dd5aa4f4fc1d74476c046e
Mq930 <118762764+Mq930@users.noreply.github.com>
2024-02-24 15:08:34 +0530

    addition of 2 numbers using scanner in java

---
+++ add2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class scan
+{
+ public static void main(String[]args)
+ {
+  Scanner sc=new Scanner(System.in);
+  System.out.print("Enter ur 1st integer= ");
+  int a= sc.nextInt();
+  System.out.print("Enter ur 2nd integer= ");
+  int b= sc.nextInt();
+  System.out.println("SUM="+(a+b));
+}
+}
