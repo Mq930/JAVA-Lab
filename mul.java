6d003b83dd7eb91eb42d5259a2ec8e3c7e75512c
Mq930 <118762764+Mq930@users.noreply.github.com>
2024-02-24 15:25:44 +0530

    Multiplication of 2 integers using scanner in java

---
+++ mul.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class scan
+{
+ public static void main(String[]args)
+ {
+  Scanner sc=new Scanner(System.in);
+  System.out.print("Enter 1st integer= ");
+  int a= sc.nextInt();
+  System.out.print("Enter 2nd integer= ");
+  int b= sc.nextInt();
+  System.out.println("PRODUCT="+(a*b));
+}
+}
