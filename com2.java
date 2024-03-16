a953cbf3188ef76e66c4d475def94a252d07f0f4
Mq930 <118762764+Mq930@users.noreply.github.com>
2024-03-16 14:39:08 +0530

    Command Line Arguments

---
+++ com2.java
@@ -0,0 +1,10 @@
+class com2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Welcome "+args[0]);
+		int a=Integer.parseInt(args[1]);
+		int b=Integer.parseInt(args[2]);
+		System.out.println(a+b);
+	}
+}
